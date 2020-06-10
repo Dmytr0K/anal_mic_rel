da76e8612511ae099f5da6bdcea1cdfb18e7ca7f
Dmytr0K <dkukhno@gmail.com>
2020-06-10 23:09:52 +0400

    added gitignore

--- cmake-build-debug/CMakeFiles/anal_mic_rel.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/anal_mic_rel.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/anal_mic_rel.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
